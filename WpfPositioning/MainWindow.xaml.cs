07f5ba76ceb39a39ae25880601d2e0c1f22a9d5a
Jon Campbell <joncamp@microsoft.com>
2018-06-21 14:18:15 -0700

    Fix DPI Scaling

--- WpfPositioning/MainWindow.xaml.cs
+++ WpfPositioning/MainWindow.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using EyeXFramework;
 using System;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 
 namespace WpfPositioning
@@ -18,6 +21,9 @@ namespace WpfPositioning
 
         Rect screenSize;
 
+        uint rawDpiX;
+        uint rawDpiY;
+
         // converts mm to pixels
         double conversionFactorX;
         double conversionFactorY;
@@ -31,11 +37,14 @@ namespace WpfPositioning
 
         private void Window_Loaded(object sender, RoutedEventArgs routedEventArgs)
         {
-            var dpiScale = VisualTreeHelper.GetDpi(this);
-            conversionFactorX = dpiScale.PixelsPerInchX / 25.4;
-            conversionFactorY = dpiScale.PixelsPerInchY / 25.4;
+            FetchRawDpi(out rawDpiX, out rawDpiY);
 
             screenSize = SystemParameters.WorkArea;
+
+            var dpiScale = VisualTreeHelper.GetDpi(this);
+
+            conversionFactorX = (rawDpiX / dpiScale.DpiScaleX) / 25.4;
+            conversionFactorY = (rawDpiY / dpiScale.DpiScaleY) / 25.4;
 
             D40.Width = conversionFactorX * 40;
             D40.Height = conversionFactorY * 40;
@@ -59,7 +68,7 @@ namespace WpfPositioning
                     double leftEyeY = (screenSize.Height / 2) + verticalOffset - (e.LeftEye.Y * conversionFactorY);
                     double rightEyeX = (screenSize.Width / 2) + (e.RightEye.X * conversionFactorX);
                     double rightEyeY = (screenSize.Height / 2) + verticalOffset - (e.RightEye.Y * conversionFactorY);
-                    
+
                     double leftEyeZ = e.LeftEye.Z;
                     double rightEyeZ = e.RightEye.Z;
 
@@ -97,25 +106,69 @@ namespace WpfPositioning
             };
         }
 
+        private void FetchRawDpi(out uint rawDpiX, out uint rawDpiY)
+        {
+            WindowInteropHelper windowInteropHelper = new WindowInteropHelper(this);
+            var screen = Screen.FromHandle(windowInteropHelper.Handle);
+
+            var point = new System.Drawing.Point(screen.Bounds.Left + 1, screen.Bounds.Top + 1);
+            var monitor = MonitorFromPoint(point, 2/*MONITOR_DEFAULTTONEAREST*/);
+            GetDpiForMonitor(monitor, DpiType.Raw, out rawDpiX, out rawDpiY);
+        }
+
         private void ExitButton_Click(object sender, RoutedEventArgs routedEventArgs)
         {
             Close();
         }
 
-        private void Window_KeyDown(object sender, KeyEventArgs e)
+        private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
-            if (e.Key == Key.F12)
+            switch(e.Key)
             {
-                if (StatusTextBlock.Visibility == Visibility.Visible)
-                {
-                    StatusTextBlock.Visibility = Visibility.Collapsed;
-                }
-                else // if (StatusTextBlock.Visibility == Visibility.Collapsed)
-                {
-                    StatusTextBlock.Visibility = Visibility.Visible;
-                }
+                case Key.F12:
+                    if (StatusTextBlock.Visibility == Visibility.Visible)
+                    {
+                        StatusTextBlock.Visibility = Visibility.Collapsed;
+                    }
+                    else // if (StatusTextBlock.Visibility == Visibility.Collapsed)
+                    {
+                        StatusTextBlock.Visibility = Visibility.Visible;
+                    }
+                    break;
+                case Key.System:
+                    if (e.SystemKey == Key.F10)
+                    {
+                        if (D100.Visibility == Visibility.Visible)
+                        {
+                            D100.Visibility = Visibility.Collapsed;
+                            D40.Visibility = Visibility.Collapsed;
+                            D4.Visibility = Visibility.Collapsed;
+                        }
+                        else // if (D100.Visibility == Visibility.Collapsed)
+                        {
+                            D100.Visibility = Visibility.Visible;
+                            D40.Visibility = Visibility.Visible;
+                            D4.Visibility = Visibility.Visible;
+                        }
+                    }
+                    break;
             }
         }
 
+        //https://msdn.microsoft.com/en-us/library/windows/desktop/dd145062(v=vs.85).aspx
+        [DllImport("User32.dll")]
+        private static extern IntPtr MonitorFromPoint([In]System.Drawing.Point pt, [In]uint dwFlags);
+
+        //https://msdn.microsoft.com/en-us/library/windows/desktop/dn280510(v=vs.85).aspx
+        [DllImport("Shcore.dll")]
+        private static extern IntPtr GetDpiForMonitor([In]IntPtr hmonitor, [In]DpiType dpiType, [Out]out uint dpiX, [Out]out uint dpiY);
+    }
+
+    //https://msdn.microsoft.com/en-us/library/windows/desktop/dn280511(v=vs.85).aspx
+    public enum DpiType
+    {
+        Effective = 0,
+        Angular = 1,
+        Raw = 2,
     }
 }
